cfbd91355b7b4526784b89249de1a605edf8f97b
Glen Chiacchieri <www@glench.com>
2021-12-29 13:16:23 -0500

    initial page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    export let prerender = true;
+</script>
+<nav></nav>
+<header>
+    <h1>The SaaS Starter Template for SvelteKit</h1>
+
+    <p>Everything you need</p>
+
+    <script async src="https://eomail1.com/form/3690e32d-68d0-11ec-96e5-06b4694bee2a.js" data-form="3690e32d-68d0-11ec-96e5-06b4694bee2a"></script>
+
+    <div class="inline-container"></div>
+</header>
+
+<style>
+    :global(.inline-container) {
+        margin: 0 auto;
+    }
+    :global(input[type="email"]) {
+        margin-bottom: 0;
+    }
+</style>
