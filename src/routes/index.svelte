4545a10d343db961015a658af35ce93b32161676
Glen Chiacchieri <www@glench.com>
2021-12-29 14:00:04 -0500

    add pre-rendered page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,16 @@
-<script context="module">
-    export let prerender = true;
-</script>
-<nav></nav>
-<header>
-    <h1>The SaaS Starter Template for SvelteKit</h1>
+<svelte:head>
+    <title>Svelte SaaS: The SaaS Template for SvelteKit</title>
+</svelte:head>
 
-    <p>Everything you need</p>
+<nav>
+</nav>
+
+<header>
+    <small>Coming soon...</small>
+    <h1>✨ The SaaS Template for SvelteKit ✨</h1>
+
+
+    <p style="width: var(--width-card); display: inline-block; vertical-align: top; text-align: left; margin-right: 3rem">Everything you need to launch your next app.</p>
 
     <script async src="https://eomail1.com/form/3690e32d-68d0-11ec-96e5-06b4694bee2a.js" data-form="3690e32d-68d0-11ec-96e5-06b4694bee2a"></script>
 
@@ -14,9 +19,13 @@
 
 <style>
     :global(.inline-container) {
+        display: inline-block !important;
         margin: 0 auto;
     }
     :global(input[type="email"]) {
         margin-bottom: 0;
+    }
+    :global(.mastfoot) {
+        display: none !important;
     }
 </style>
