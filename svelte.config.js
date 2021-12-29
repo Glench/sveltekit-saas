03b24a9554f724ee9d99f5466f35eaf2484960d4
Glen Chiacchieri <www@glench.com>
2021-12-29 14:16:22 -0500

    build

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
@@ -8,6 +10,9 @@ const config = {
     preprocess: preprocess(),
 
     kit: {
+        paths: {
+            base: dev ? '' : '/svelte-saas',
+        },
         adapter: adapter({
             pages: 'docs',
             assets: 'docs',
