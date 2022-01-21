d67221c8254f246423133736cbd5dd36441b871c
Glen Chiacchieri <www@glench.com>
2022-01-21 11:23:58 -0500

    updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,47 +11,156 @@
     </script>
 </svelte:head>
 
-<nav>
-</nav>
+    <header>
+        <div style="max-width: 550px; padding: 1rem;">
+            <h1>Launch faster with the <span class="gradient">Svelte SaaS Starter Kit</span></h1>
 
-<header>
-    <small>Coming soon...</small>
-    <h1>✨ The Full-Stack SaaS Template for SvelteKit ✨</h1>
+            <p>A full-stack SaaS template built on SvelteKit and Node.js that starts your project with all the features that are the same in every app.</p>
+
+            <p>Focus on the unique parts of your app, save weeks of tedious coding, and launch faster. 🚀</p>
+        </div>
 
 
-    <div style="width: 350px; display: inline-block; vertical-align: top; text-align: left; margin-right: 3rem">
-        <p>Everything you need to launch your next app.</p>
-        
-        <p>User auth, admin dashboards, Stripe payments, and more — all built on top of the best in class SvelteKit framework.</p>
+        <form>
+            <script async src="https://eomail1.com/form/3690e32d-68d0-11ec-96e5-06b4694bee2a.js" data-form="3690e32d-68d0-11ec-96e5-06b4694bee2a"></script>
+        </form>
 
-        <p>Stop coding the same features in every app and save weeks of development time.</p>
-    </div>
+    </header>
+    <section style="background: radial-gradient(50% 50% at 50% 50%, #6E7176 0%, #515D6A 100%); padding: 4rem 1.5rem;">
+        <div class="card" style="border-radius:6px;">
+            <h2 style="color: white;">Svelte SaaS 🚀</h2>
+            <ul class="emoji">
+                <li>User signup and auth</li>
+                <li>Admin dashboards</li>
+                <li>Stripe subscriptions</li>
+                <li>Database models, forms, REST APIs, and more</li>
+            </ul>
+        </div>
 
-    <script async src="https://eomail1.com/form/3690e32d-68d0-11ec-96e5-06b4694bee2a.js" data-form="3690e32d-68d0-11ec-96e5-06b4694bee2a"></script>
+        <div class="card">
+            <h2>SvelteKit 🔥</h2>
+            <ul>
+                <li>Optimized full-stack apps with no configuration</li>
+                <li>SEO and performance from server-side rendering</li>
+                <li>Zippy UX from client-side hydration &amp; optional routing
+                <li>Stay in dev flow with instant hot-module reloading</li>
+            </ul>
+        </div>
 
-</header>
-<div style="height: 100px; border-bottom: 4px solid #aaa;">
-
-</div>
+ 
+        <div class="card">
+            <h2>Svelte ❤️</h2>
+            <ul>
+                <li>Blazing fast UI components &mdash; no virtual DOM</li>
+                <li>Tiny builds &mdash; no bloated JS</li>
+                <li>Voted <a href="https://insights.stackoverflow.com/survey/2021#section-most-loved-dreaded-and-wanted-web-frameworks">most-loved framework in the Stack Overflow developer survey</a> (beating out React, Rails, Django, and more!)</li>
+            </ul>
+        </div>
+    </section>
 
 <style>
     :global(body) {
-        background-color: rgb(223, 223, 223) !important;
+        background-color: white;
+        color: rgba(77, 77, 77, 1);
+        font-family: Inter, sans-serif;
+        line-height: 1.5;
+        margin: 0;
+        overflow-x: hidden;
+        padding: 0;
     }
-    :global(#svelte) {
-        background-color: var(--color-bg) !important;
-    }
-    :global(.inline-container) {
-        display: inline-block !important;
-        margin: 0 auto;
+    :global(.btn) {
+        flex-grow: 0 !important;
     }
     :global(input[type="email"]) {
         margin-bottom: 0;
     }
-    :global(.mastfoot) {
+    :global(.mastfoot) { /* remove email octopus branding */
         display: none !important;
     }
     :global(.emailoctopus-success-message) {
         color: #3ab73a !important;
+    }
+
+    section a {
+        color: inherit;
+    }
+    header {
+        max-width: 1080px;
+        align-items: center; /* vertical centering */
+        gap: 3rem;
+    }
+    header, section {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        margin: 2rem auto;
+        padding: 1.5rem;
+    }
+    h1 {
+        font-family: Inter, sans-serif;
+        font-size: 3rem;
+        color: black;
+        line-height: 1.2;
+    }
+    h1 span {
+        white-space: nowrap;
+        display: block;
+    }
+    .gradient {
+        -webkit-background-clip: text;
+        -webkit-box-decoration-break: clone;
+        -webkit-font-smoothing: subpixel-antialiased;
+        -webkit-text-fill-color: transparent;
+        background-image: linear-gradient(180deg, #FF3E00 43.62%, #640000 100%);
+        background-attachment: scroll;
+        background-clip: text;
+        background-color: rgba(0,0,0,0);
+    }
+    p {
+        font-size: 1.15rem;
+    }
+    form {
+        border: 1px solid #e9e9e9;
+        border-radius: 5px;
+        box-shadow: rgb(244, 244, 244) 2px 2px 10px 0px;
+        box-sizing: border-box;
+        color: rgb(0, 0, 0);
+        display: block;
+        padding: 1.7rem;
+        width: 361px;
+    }
+    .card {
+        max-width: 280px;
+        color: rgba(198, 211, 222, 1);
+        background-color: #2A3643;
+        padding: 1.75rem;
+        margin: 2rem;
+        border-radius: 8px;
+        box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.25);
+    }
+    .card h2 {
+        padding: 0;
+        margin: 0;
+        font-size: 2rem;
+    }
+    .card ul {
+        margin: 0 0 0 1rem;
+        padding: 0;
+    }
+    .card li {
+        margin: 1rem 0;
+    }
+    .card ul.emoji {
+        list-style: none;
+        color: white;
+    }
+    .card ul.emoji li {
+        font-size: 1.2rem;
+        text-indent: -2rem;
+        padding-left: 1rem;
+    }
+    .card ul.emoji li:before {
+        content: "⭐️";
+        margin-right: .5rem;
     }
 </style>
