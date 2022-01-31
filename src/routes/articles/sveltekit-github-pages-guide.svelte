d75dcb47c946b0a2cf7125b20b97c94a680e3145
Glen Chiacchieri <www@glench.com>
2022-01-31 09:43:23 -0500

    fix image names and alt tags

--- src/routes/articles/sveltekit-github-pages-guide.svelte
+++ src/routes/articles/sveltekit-github-pages-guide.svelte
@@ -45,7 +45,7 @@
             <strong>Choose a license</strong>
             : Optional. Choose any license you want, or none. You can always add one later if you’re not sure.</li></ul>
     <figure id="24fd0473-7338-4054-a445-14c8d0fbda52" class="image">
-        <img style="width:748px" src="{assets}/articles/sveltekit-github-pages-guide/Screen_Shot_2022-01-22_at_11.51.48_AM.png"/>
+        <img style="width:748px" src="{assets}/articles/sveltekit-github-pages-guide/github_new_repo_settings.png" alt="Screenshot of Github's new repository settings page"/>
     </figure>
     <p id="04252290-fdab-4b91-b78a-eebcedd563b1" class="">Once you’re happy with your settings, click the <strong>Create repository</strong> button and you’ll have a new blank Github repository.</p>
     <p id="8acb4fdd-b375-4ffb-bbcb-ad5855d240f3" class="">To get the repository on your computer, you can run <code>git clone &lt;repository url&gt;</code> in a terminal or use your favorite git software.</p>
@@ -72,16 +72,23 @@
         <code>npm run dev -- --open</code>
     </pre>
 
-    <figure id="d493864a-3b47-4a5e-b581-3e72f2b3e074" class="image"><img style="width:528px" src="{assets}/articles/sveltekit-github-pages-guide/Screen_Shot_2022-01-26_at_3.09.19_PM.png"/></figure>
+    <figure id="d493864a-3b47-4a5e-b581-3e72f2b3e074" class="image"><img style="width:528px" src="{assets}/articles/sveltekit-github-pages-guide/sveltekit_skeleton_page.png" alt="Screenshot of SvelteKit's default skeleton page"/></figure>
 
     <p id="28acdcab-862b-49e8-a362-aa573d9a4ed9" class="">Yay!</p>
     <h2 id="80c176b7-13fe-482d-a0ec-ae33c89e983e" class="">Configure Github Pages</h2>
     <p id="156b17e7-2797-4a0f-8e9d-25a3f1d2c831" class="">Github Pages only allows sites to be hosted from either the root of a repository (<code>/</code>)  or a <code>/docs</code> folder. Sites can also be hosted from any branch of a repository, but again, only in the root or <code>/docs</code> folder.</p>
     <p id="db8ca798-b74c-431a-82e3-001529bda0cf" class="">To keep things simple and clean, we’re going to put our static site in a <code>/docs</code> folder in the <code>main</code> branch.</p>
     <p id="25d1ec3c-d8d1-4bc0-b025-47b9495af03e" class="">In your Github repository settings, navigate to the <code>Pages</code> section. It should look something like this:</p>
-    <figure id="b753dfad-8436-4e5f-80a9-f2b0eaf801ac" class="image"><img style="width:1137px" src="{assets}/articles/sveltekit-github-pages-guide/Screen_Shot_2022-01-26_at_3.44.45_PM.png"/></figure>
+
+    <figure id="b753dfad-8436-4e5f-80a9-f2b0eaf801ac" class="image">
+        <img style="width:1137px" src="{assets}/articles/sveltekit-github-pages-guide/github_pages_settings.png" alt="Screenshot of Github Pages' settings"/>
+    </figure>
+
     <p id="4867b6c2-cbc3-45df-99b0-3cc849d8672a" class="">In the “Source” section there’s a dropdown that currently says “None”. Instead of None, choose the <code>main</code> branch and then the <code>/docs</code> folder, as shown below.</p>
-    <figure id="b7f4648d-760f-41be-a503-1cb6c3e95d92" class="image"><img style="width:396px" src="{assets}/articles/sveltekit-github-pages-guide/Screen_Shot_2022-01-24_at_4.11.13_PM.png"/></figure>
+
+    <figure id="b7f4648d-760f-41be-a503-1cb6c3e95d92" class="image">
+        <img style="width:396px" src="{assets}/articles/sveltekit-github-pages-guide/github_pages_branch_settings.png" alt="Screenshot of Github Pages' branch and folder settings"/>
+    </figure>
 
     <p id="eb829a65-531b-4d46-b87e-abc4bf765e20" class="">Click “Save” and the page will reload and tell you that your site is ready to be served.</p>
     <h2 id="35ace530-b5da-41e7-8b7f-58ac7f83983e" class="">Configure SvelteKit for Github Pages</h2>
@@ -91,7 +98,7 @@
     <p id="056fe096-d44c-4fbd-87ef-b80503b0aa9f" class="">(You can also run <code>npm uninstall @sveltejs/adapter-auto --save-dev</code> to remove the unnecessary auto adapter from your package if you want.)</p>
     <p id="3dfd944b-2358-41c2-85ce-49a85b94c729" class="">Edit <code>svelte.config.js</code> to use the static adapter as shown below:</p>
     <pre id="84751aaf-8bfb-4976-80b4-1904436c8d13" class="code"><code>{`import adapter from "@sveltejs/adapter-static"; 
-// was adapter-auto
+// was "@sveltejs/adapter-auto"
 
 const dev = process.env.NODE_ENV === "development";
 
@@ -131,10 +138,13 @@ export default config;`}</code></pre>
     <p id="2a015488-106d-427d-b92a-5b83650d604c" class="">...and push them to Github...</p>
     <pre id="11b05ad5-ee07-41f2-ba63-19f29457082e" class="code"><code>git push origin main</code></pre>
     <p id="40e3a791-f191-4170-8cde-193958327d50" class="">Github will automatically deploy your site! In the Github Pages settings you’ll see this right after you push:</p>
-    <figure id="b60bdc1a-65a8-4fe2-b259-9f167c189573" class="image"><img style="width:821px" src="{assets}/articles/sveltekit-github-pages-guide/Screen_Shot_2022-01-26_at_4.14.50_PM.png"/></figure>
+
+    <figure id="b60bdc1a-65a8-4fe2-b259-9f167c189573" class="image">
+        <img style="width:821px" src="{assets}/articles/sveltekit-github-pages-guide/github_pages_site_ready.png" alt="Screenshot of Github Pages' settings with site ready to be published"/>
+    </figure>
     <p id="3e29ec3b-09bb-4e3f-97ec-86cffc405016" class="">If you wait a little bit, eventually that page will look like this:</p>
 
-    <figure id="324e7a08-fe62-4257-9b62-fb4cb95e55db" class="image"><img style="width:794px" src="{assets}/articles/sveltekit-github-pages-guide/Screen_Shot_2022-01-26_at_3.42.45_PM.png"/></figure>
+    <figure id="324e7a08-fe62-4257-9b62-fb4cb95e55db" class="image"><img style="width:794px" src="{assets}/articles/sveltekit-github-pages-guide/github_pages_site_published.png"/></figure>
 
     <p id="5bd45d46-037c-4c28-a337-b15882a1395e" class="">Click the link to see your new site running with Github Pages!</p>
     <p id="aa3ab243-fa7e-462b-bd98-977c5b31c220" class="">That is generally the process every time you make a change:</p>
@@ -149,8 +159,11 @@ export default config;`}</code></pre>
     <p id="0863c72e-a475-442a-b196-e9550fba2af7" class="">The main thing you’ll need in your SvelteKit repository is a <code>CNAME</code> file containing the URL of the site you want:</p>
     <pre id="165ab509-27a2-4574-b10f-39ca60c6dedf" class="code"><code>yourcustomdomain.com</code></pre>
     <p id="3dbc643f-8d0e-408d-8ff8-d0a509e6dd75" class="">Put this <code>CNAME</code> file in the <code>static</code> directory of your SvelteKit project and you’re good to go!</p>
+
     <h3 id="4fa0fbe3-0293-4b0d-abae-3f4dc391f190" class="">Addendum: Getting HTTP, HTTPS, and WWW working with custom domains on Github</h3>
-    <p id="2c381a06-3018-469e-a6c1-8cd53b4c9ab3" class="">This isn’t a SvelteKit-specific thing but it’s important to understand how to configure Github Pages so that all the following custom domain URLs will work:</p>
+
+    <p id="2c381a06-3018-469e-a6c1-8cd53b4c9ab3" class="">This isn’t a SvelteKit-specific thing but it’s a little confusing how to properly configure DNS for Github Pages so that all the following custom domain URLs will work:</p>
+
     <pre id="1c6756dd-b461-4e81-be9d-66be45d188fb" class="code"><code>example.com
 www.example.com
 http://example.com
@@ -158,6 +171,6 @@ http://www.example.com
 https://example.com
 https://www.example.com</code></pre>
     <p id="f22f182c-2ff1-47da-8571-1e8842a46c31" class="">To make all those work, it’s pretty simple. Just add “A Records” pointing toward Github’s IP addresses, as well as a “CNAME Record” for www pointed at <code>username.github.io</code>:</p>
-    <figure id="b7650a03-f098-4ab7-adbf-e8317567d736" class="image"><img style="width:542px" src="{assets}/articles/sveltekit-github-pages-guide/Screen_Shot_2022-01-27_at_3.14.47_PM.png"/></figure>
+    <figure id="b7650a03-f098-4ab7-adbf-e8317567d736" class="image"><img style="width:542px" src="{assets}/articles/sveltekit-github-pages-guide/github_pages_dns_settings.png" alt="Screenshot of DNS settings for Github custom domain"/></figure>
     <p id="3ec5ffaa-8522-477f-9e13-2a108c9878b6" class="">You should also choose “Enforce HTTPS” from the Github Pages settings page. With this setup, every URL from the list above will automatically go to <code>https://example.com</code>.</p>
 
